874ddd8f83d752bb9f58ae0e6fbaa04e7312aafa
sep <sepbot@gmail.com>
2024-10-05 21:59:20 +1000

    fix test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,3 +1,4 @@
+import * as console from "node:console";
 import { DockerClient } from "~/index.ts";
 
 test("DockerClient", async () => {
@@ -13,10 +14,16 @@ test("DockerClient", async () => {
     },
   });
 
+  // TODO: is there a better way, do we have to use containerd?
+  let found = false;
+  while (!found) {
+    const resp = await client.Image.List({});
+    found = resp.find((i) => i.RepoTags?.includes('hello-world:latest')) !== undefined;
+  }
+
   const createResp = await client.Container.Create({
     body: {
       Image: "hello-world",
-
     },
   });
 
